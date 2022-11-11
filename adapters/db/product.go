b117d38a6783d589d68e271d2fff0fee420f5e72
Igor Marchi <igoraugustomarchi@gmail.com>
2022-11-10 21:03:53 -0300

    create db test

---
+++ adapters/db/product.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/igor-marchi/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
+func (p *ProductDb) Get(id string) (application.IProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("select id, name, price, status from product where id = ?")
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
---
+++ adapters/db/product_test.go
@@ -0,0 +1,61 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/igor-marchi/go-hexagonal/adapters/db"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE product (
+		"id" string,
+		"name" string,
+		"price" string,
+		"status" string
+	)`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO product VALUES (
+		"abc",
+		"Product Test",
+		0,
+		"disabled"
+	)`
+
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
